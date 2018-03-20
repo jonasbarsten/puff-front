1249974b33d506e51042d677d0686125b3b3d956
Jonas Barsten <jonasbarsten@gmail.com>
2018-03-20 16:46:52 +0100

    Pulling files from RPi with initial sketches

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false
+    };
+  }
+  componentDidMount() {
+    // For some reason, cannot use 127.0.0.1
+    const endpoint = window.location.hostname + ':4001';
+    // const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("FromAPI", data => this.setState({ response: data }));
+  }
   render() {
+    const { response } = this.state;
+
+    console.log(response);
+    const scaledWidth = (response.velocity/127) * 100;
+
+    const width = `${scaledWidth}%`;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{ textAlign: "center" }}>
+        {response
+          ? 
+          <div className="jumbotron">
+            <div className="container">
+              <div className="row">
+                <div className="col">
+                  <p>Piezo 1</p>
+                </div>
+                <div className="col-10">
+                  <div className="progress">
+                    <div 
+                      className="progress-bar" 
+                      role="progressbar" 
+                      style={{width: width}} 
+                      aria-valuenow={response.velocity} 
+                      aria-valuemin="0" 
+                      aria-valuemax="127"
+                    >
+                      {response.velocity} / 127
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          : <p>Loading...</p>}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import '../node_modules/jquery/dist/jquery.min.js';
+import '../node_modules/popper.js/dist/popper.min.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
